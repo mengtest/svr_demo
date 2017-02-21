9ce1113e55937dd6fe66e41445b5b8977c75deb4
lzz <344168752@qq.com>
2017-02-21 19:56:16 +0800

    logger

---
+++ script/logger.lua
@@ -0,0 +1,24 @@
+local skynet = require "skynet"
+
+--打印table
+function print_t(root)
+	local cache = {  [root] = "." }
+	local function _dump(t,space,name)
+		local temp = {}
+		for k,v in pairs(t) do
+			local key = tostring(k)
+			if cache[v] then
+				table.insert(temp,"+" .. key .. " {" .. cache[v].."}")
+			elseif type(v) == "table" then
+				local new_key = name .. "." .. key
+				cache[v] = new_key
+				table.insert(temp,"+" .. key .. _dump(v,space .. (next(t,k) and "|" or " " ).. 
+				string.rep(" ",#key),new_key))
+			else
+				table.insert(temp,"+" .. key .. " [" .. tostring(v).."]")
+			end
+		end
+		return table.concat(temp,"\n"..space)
+	end
+	print(_dump(root, "",""))
+end
